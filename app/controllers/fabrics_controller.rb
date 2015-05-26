70d82e830a6a17df3e85804461a0ad55e3f9d74f
Valerie Smith <valeriesmithdbc@gmail.com>
2015-05-26 13:57:25 -0700

    creates template for displaying fabric by id adds ability to add new fabric without file upload

--- app/controllers/fabrics_controller.rb
+++ app/controllers/fabrics_controller.rb
@@ -3,11 +3,22 @@ class FabricsController < ApplicationController
 	end
 
 	def create
-		@fabric = Fabric.new(params.require(:fabric) .permit(:name,:color,:length,:width,:price,:content,:source,:swatch))
+		@fabric = Fabric.new(fabric_params)
 
 		@fabric.save
 		redirect_to @fabric
 	end
+
+	def show
+		@fabric = Fabric.find(params[:id])
+	end
+
+	private
+
+		def fabric_params
+			params.require(:fabric) .permit(:name,:color,:length,:width,:price,:content,:source,:swatch)
+		end
+
 end
 
 
