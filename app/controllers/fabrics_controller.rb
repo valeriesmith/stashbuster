40c16d64e66da6878e8a2bee23f96890b1f7d967
Valerie Smith <valeriesmithdbc@gmail.com>
2015-05-26 13:38:05 -0700

    adds image display

--- app/controllers/fabrics_controller.rb
+++ app/controllers/fabrics_controller.rb
@@ -1,2 +1,13 @@
 class FabricsController < ApplicationController
+	def new
+	end
+
+	def create
+		@fabric = Fabric.new(params.require(:fabric) .permit(:name,:color,:length,:width,:price,:content,:source,:swatch))
+
+		@fabric.save
+		redirect_to @fabric
+	end
 end
+
+
